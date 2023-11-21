353b9bc2e584e328d25cea2a51c6f1206e2f65dc
Laurent Cimon <laurent@nilio.ca>
2023-11-21 12:52:01 -0500

    Add fortune.py

---
+++ fortune.py
@@ -0,0 +1,41 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QPushButton, QVBoxLayout
+
+import sys
+import subprocess
+import platform
+
+system = platform.system()
+if system == 'OpenBSD':
+    fortune = '/usr/games/fortune'
+else:
+    fortune = 'fortune'
+
+def get_fortune():
+    process = subprocess.Popen([fortune], stdout=subprocess.PIPE)
+    output, _ = process.communicate()
+    return output.decode()
+
+def start_window():
+    app = QApplication(sys.argv)
+
+    mainwidget = QWidget()
+    mainwidget.setWindowTitle("fortune")
+
+    layout = QVBoxLayout()
+    mainwidget.setLayout(layout)
+
+    label = QLabel(get_fortune())
+    label.setAlignment(Qt.AlignCenter)
+    def set_fortune():
+        label.setText(get_fortune())
+
+    button = QPushButton("Get Fortune")
+    button.clicked.connect(set_fortune)
+
+    layout.addWidget(label)
+    layout.addWidget(button)
+
+    mainwidget.show()
+    app.exec()
+
+start_window()
