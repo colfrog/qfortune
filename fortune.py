87759bce7c474c0a3a5c141aa29a3479aac23ef6
Laurent Cimon <laurent@nilio.ca>
2023-11-22 13:14:09 -0500

    Remove QMainWindow from the imports

--- fortune.py
+++ fortune.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
 
 import sys
 import subprocess
